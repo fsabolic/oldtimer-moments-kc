db652c9c9b26eeaa43573d8c3ee655be4e79f00d
Fran Sabolić <fransabolicm8@gmail.com>
2025-12-07 22:17:04 +0100

    Vite config fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
-import devtools from 'solid-devtools/vite';
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import devtools from "solid-devtools/vite";
 
 export default defineConfig({
   plugins: [devtools(), solidPlugin()],
+  base: "/oldtimer-moments-kc/",
   server: {
     port: 3000,
   },
   build: {
-    target: 'esnext',
+    target: "esnext",
   },
 });
